99000965b0248c1cf452c6c9ff28cbd4c70e4915
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-16 15:13:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let precioInicial: number = 450.5;
+let porcentajeDescuento: number = 0.1;
+let precioFinal: number = precioInicial - precioInicial * porcentajeDescuento;
+
+console.log("El precio final es de: $", precioFinal);
